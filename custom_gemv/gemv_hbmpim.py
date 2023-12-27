e3f60c63e11f720c8865109883bfa61852fd07a0
Yongwon Shin <ywshin@postech.ac.kr>
2023-12-27 21:10:58 +0900

    modify hbmpim script

--- custom_gemv/gemv_hbmpim.py
+++ custom_gemv/gemv_hbmpim.py
@@ -1,4 +1,4 @@
-mport tvm
+import tvm
 import tvm.testing
 from tvm import te
 import numpy as np
@@ -18,19 +18,26 @@ N_GRF_A = 8
 N_GRF_B = 8
 N_ELEM_IN_GRF = 16
 
+
+class BankOrdering:
+    DENSE = 1
+    EVEN = 2
+    ODD = 3
+
+
 N_OUT_TILE = N_CHAN * N_PU * N_GRF_B
 N_IN_TILE = N_GRF_A * N_ELEM_IN_GRF
 
 # The size of the matrix
 # (M, K) x (K, N)
-M = 4096*2
+M = 4096 * 2
 K = 1024
 N = 1
 
 log = []
 
 # The default tensor data type in tvm
-dtype = "float32"
+dtype = "float16"
 
 target = tvm.target.Target(target="hbmpim", host="llvm")
 dev = tvm.device(target.kind.name, 0)
@@ -38,9 +45,15 @@ dev = tvm.device(target.kind.name, 0)
 
 # Random generated tensor for testing
 a = tvm.nd.array(np.random.rand(M, K).astype(dtype), dev)
-b = tvm.nd.array(np.random.rand(K,).astype(dtype), dev)
+b = tvm.nd.array(
+    np.random.rand(
+        K,
+    ).astype(dtype),
+    dev,
+)
 
 answer = np.dot(a.numpy(), b.numpy())
+
 
 @tvm.script.ir_module
 class HBMPIMModule:
@@ -83,7 +96,9 @@ class HBMPIMModule:
                 with T.init():
                     pass
                     # P[v_i, v_r] = 0
-                P[v_i, v_r] = P[v_i, v_r] + A[v_i, v_k*N_ELEM_IN_GRF + v_r] * B[v_k*N_ELEM_IN_GRF + v_r]
+                P[v_i, v_r] = (
+                    P[v_i, v_r] + A[v_i, v_k * N_ELEM_IN_GRF + v_r] * B[v_k * N_ELEM_IN_GRF + v_r]
+                )
         for i, r in T.grid(M, N_ELEM_IN_GRF):
             with T.block("C"):
                 v_i, v_r = T.axis.remap("SR", [i, r])
@@ -92,9 +107,11 @@ class HBMPIMModule:
                     # C[v_i] = 0
                 C[v_i] = C[v_i] + P[v_i, v_r]
 
+
 ir_module = HBMPIMModule
 print(type(ir_module))
 print(ir_module.script())
+
 
 def te_to_tir():
     # TVM Matrix Vector Multiplication using TE
@@ -107,7 +124,9 @@ def te_to_tir():
     ir_module_from_te = IRModule({"main": func})
     print(ir_module_from_te.script())
 
+
 # te_to_tir()
+
 
 def evaluate_operation(func, log):
     c = tvm.nd.array(np.zeros((M,), dtype=dtype), dev)
@@ -118,6 +137,7 @@ def evaluate_operation(func, log):
     evaluator = func.time_evaluator(func.entry_name, dev, number=10)
     mean_time = evaluator(a, b, c, p).mean
     log.append(("runtime", mean_time))
+
 
 # schedule
 sch = tvm.tir.Schedule(ir_module)
@@ -144,17 +164,24 @@ sch.tensorize(ax_p, HBMPIM_PARTIAL_REDUCTION_INTRIN)
 # sch.vectorize(r)
 sch.bind(i_1, "blockIdx.x")
 sch.bind(i_2, "puIdx.x")
-sch.annotate(k_0, "bank", True)
+sch.annotate(i_1, "bank", BankOrdering.DENSE)
+sch.annotate(i_2, "bank", BankOrdering.DENSE)
+sch.annotate(k_0, "bank", BankOrdering.DENSE)
 sch.annotate(i_0, "pim", True)
+sch.annotate(i_3, "barrier", True)
 
 block_c = sch.get_block("C")
 i, r = sch.get_loops(block_c)
 # cache_read_p = sch.cache_read(block_c, 0, "local")
-i_0, i_1, i_2 = sch.split(i, factors=[None, N_CHAN, N_CHAN])
-sch.reorder(i_1, i_0, i_2)
+i_0, i_1, i_2, i_3 = sch.split(i, factors=[None, N_CHAN, N_PU, N_GRF_B])
+sch.reorder(i_1, i_0, i_2, i_3)
 # sch.compute_at(cache_read_p, i_0)
 sch.bind(i_1, "blockIdx.x")
-sch.bind(i_2, "threadIdx.x")
+sch.bind(i_3, "threadIdx.x")
+# sch.bind(i_2, "puIdx.x") # NOTE: single bank mode
+sch.bind(i_2, "bankIdx.x")  # NOTE: single bank mode
+sch.annotate(i_1, "bank", BankOrdering.DENSE)
+sch.annotate(i_2, "bank", BankOrdering.ODD)
 print(sch.mod.script())
 
 # NOTE 1: 위에서 i_2가 pim_gemm.cl에서 n_out_tile에 해당하지만, i_2는 8개인 반면 n_out_tile은 1개이다. 이 차이가 발생하는 이유는 PIM block에 weight가 implicit하게 매핑되기 때문이다. 문제는 i_2가 blockIdx, threadIdx로 바인딩 될 수 없다는 것이다.
@@ -167,24 +194,24 @@ from tvm.ir.transform import PassContext, Sequential
 # Define the passes you want to apply
 # In this case, let's use the 'simplify' pass
 passes = [
-          tvm.tir.transform.StorageFlatten(64),
-          tvm.tir.transform.LowerInitBlock(),
-          tvm.tir.transform.PlanAndUpdateBufferAllocationLocation(),
-          tvm.tir.transform.ConvertBlocksToOpaque(),
-          tvm.tir.transform.CompactBufferAllocation(),
-          tvm.tir.transform.Simplify(),
-          tvm.tir.transform.UnifyThreadBinding(),
-          tvm.tir.transform.LowerMatchBuffer(),
-        #   tvm.tir.transform.LowerOpaqueBlock(),
-        #   tvm.tir.transform.FlattenBuffer(),
-        #   tvm.tir.transform.Simplify(),
+    tvm.tir.transform.StorageFlatten(64),
+    tvm.tir.transform.LowerInitBlock(),
+    tvm.tir.transform.PlanAndUpdateBufferAllocationLocation(),
+    tvm.tir.transform.ConvertBlocksToOpaque(),
+    tvm.tir.transform.CompactBufferAllocation(),
+    tvm.tir.transform.Simplify(),
+    #   tvm.tir.transform.LowerMatchBuffer(),
+    #   tvm.tir.transform.UnifyThreadBinding(),
+    #   tvm.tir.transform.LowerOpaqueBlock(),
+    #   tvm.tir.transform.FlattenBuffer(),
+    #   tvm.tir.transform.Simplify(),
 ]
 
 # Apply the passes
-with PassContext():
-    lowered_module = Sequential(passes)(sch.mod)
+# with PassContext():
+#     lowered_module = Sequential(passes)(sch.mod)
 
-print(lowered_module.script())
+# print(lowered_module.script())
 
 func = tvm.build(sch.mod, target=target, name="gemm")
 # evaluate_operation(func, log=log)
