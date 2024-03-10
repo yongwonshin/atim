a1d8342309e0ba7c30c468e0f84a2cee240b8c43
Yongwon Shin <ywshin@postech.ac.kr>
2024-03-10 07:13:21 +0000

    increase timeout for stable tuning

--- tests/python/unittest/test_meta_schedule_tune_tir.py
+++ tests/python/unittest/test_meta_schedule_tune_tir.py
@@ -106,7 +106,7 @@ def test_tune_matmul_hbmpim():
             work_dir=work_dir,
             max_trials_global=1,
             num_trials_per_iter=1,
-            per_iter_timeout_sec=120,  # NOTE: timeout 에러가 조용하게 발생하는 버그가 있으니 유의해야 한다.
+            per_iter_timeout_sec=180,  # NOTE: timeout 에러가 조용하게 발생하는 버그가 있으니 유의해야 한다.
         )
         sch = ms.tir_integration.compile_tir(database, matvec, target)
         if sch is None:
