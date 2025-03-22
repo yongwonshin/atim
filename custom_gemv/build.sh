026d2e5026d5e54197024ad8e2db9ddd3bac946d
stonerdk <stkd97@gmail.com>
2025-03-22 17:41:12 +0900

    Ignore old experiment and dependency directories

--- custom_gemv/build.sh
+++
@@ -1,5 +0,0 @@
-cd ../build
-cmake .. -G Ninja
-ninja
-cd ../custom_gemv
-TVM_LOG_DEBUG=DEFAULT=2,src/target/llvm/codegen_llvm.cc=-1,src/arith/int_set.cc=-1 python3 gemv.py
--- pim_test/upmem/SimplePIM/benchmarks/red/Param.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARAM_H
-#define PARAM_H
-#include <stdlib.h>
-
-typedef uint64_t T;
-const uint32_t dpu_number = 1024; // 2432
-uint32_t print_info = 0;
-uint64_t nr_elements = 33554432;
-#endif
