b490d8af7da5c848fbbdf70e5ec57ecf4b8a3c7e
stonerdk <stkd97@gmail.com>
2025-03-29 14:24:33 +0900

    Add autotuned modules

--- evaluation/graph/plot.py
+++ evaluation/graph/plot.py
@@ -8,24 +8,22 @@ import argparse
 plt.rcParams.update({'font.family':'Arial'})
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument("--reproduced", type=bool, action="store_true", default=True)
+argparser.add_argument("--dir", type=str, default="../reproduced")
 args = argparser.parse_args()
 
-directory = "reproduced" if args.reproduced else "results"
-
-df_gptj = pd.read_csv(f"../{directory}/result_gptj.csv")
+df_gptj = pd.read_csv(f"{args.dir}/result_gptj.csv")
 df_gptj = df_gptj.fillna(0).reset_index(drop=True)
 normalization_factor = df_gptj.iloc[:, 5:8].astype(float).sum(axis=1)
 df_gptj.iloc[:, 5:] = df_gptj.iloc[:, 5:].astype(float).div(normalization_factor, axis=0)
-plot_gpt(df_gptj, f"../{directory}/plot_gpt.pdf")
+plot_gpt(df_gptj, f"{args.dir}/plot_gpt.pdf")
 
-df_poly = pd.read_csv(f"../{directory}/result_poly.csv")
+df_poly = pd.read_csv(f"{args.dir}/result_poly.csv")
 df_poly = df_poly.fillna(0).reset_index(drop=True)
 normalization_factor = df_poly.iloc[:, 5:8].astype(float).sum(axis=1)
 df_poly.iloc[:, 5:] = df_poly.iloc[:, 5:].astype(float).div(normalization_factor, axis=0)
-plot_polybench(df_poly, f"../{directory}/plot_polybench.pdf")
+plot_polybench(df_poly, f"{args.dir}/plot_polybench.pdf")
 
-df_opt = pd.read_csv(f"../{directory}/result_opt.csv")
+df_opt = pd.read_csv(f"{args.dir}/result_opt.csv")
 df_opt = df_opt.fillna(0)
 df_opt.iloc[:, 2:] = df_opt.iloc[:, 2:].astype(float)
-plot_branch_opt(df_opt, f"../{directory}/plot_branch_opt.pdf")
+plot_branch_opt(df_opt, f"{args.dir}/plot_branch_opt.pdf")
