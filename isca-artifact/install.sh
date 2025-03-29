d981fc77ffae611a58175feab571fafbea77be7c
stonerdk <stkd97@gmail.com>
2025-03-29 23:42:07 +0900

    modify isca-artifact

--- isca-artifact/install.sh
+++ isca-artifact/install.sh
@@ -5,11 +5,13 @@ set -e
 wget --no-check-certificate https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 ./llvm.sh 16 all
+ln -s $(which llvm-config-16) /usr/local/bin/llvm-config
 
 # Download ATiM
 git clone -b artifact https://github.com/yongwonshin/atim.git
 cd atim
 git submodule update --init --recursive
+chmod +x atim/evaluation/eval_setup.sh
 
 # Install other dependencies
 source "$(conda info --base)/etc/profile.d/conda.sh"
@@ -21,3 +23,5 @@ rm -rf build; mkdir build && cd build
 cp ../cmake/config.cmake .
 cmake .. -G Ninja
 ninja
+
+echo "conda activate atim-venv" >> ~/.bashrc
